fc50f358b6763413266cb3cde2f1d05a820abe8a
bobi.rival <bahmadrival@gmail.com>
2025-02-21 20:00:16 +0700

    Fixing env file

--- helper/viper.go
+++ helper/viper.go
@@ -15,6 +15,17 @@ func NewViper() *viper.Viper {
 
 	config.SetConfigFile(constant.CONFIG_FILE)
 	config.AddConfigPath(".")
+	config.AutomaticEnv()
+
+	config.BindEnv(constant.APP_NAME, constant.APP_NAME_ENV_NAME)
+	config.BindEnv(constant.DB_HOST, constant.DB_HOST_ENV_NAME)
+	config.BindEnv(constant.DB_PORT, constant.DB_PORT_ENV_NAME)
+	config.BindEnv(constant.DB_USERNAME, constant.DB_USERNAME_ENV_NAME)
+	config.BindEnv(constant.DB_PASSWORD, constant.DB_PASSWORD_ENV_NAME)
+	config.BindEnv(constant.DB_NAME, constant.DB_NAME_ENV_NAME)
+	config.BindEnv(constant.DB_SHOW_SQL, constant.DB_SHOW_SQL_ENV_NAME)
+	config.BindEnv(constant.JWT_KEY, constant.JWT_KEY_ENV_NAME)
+	config.BindEnv(constant.JWT_EXPIRED, constant.JWT_EXPIRED_ENV_NAME)
 
 	err := config.ReadInConfig()
 	PanicIfError(err)
